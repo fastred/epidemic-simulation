bd05342202ca66747775cee9058260a8f3cfc7c2
Arkadiusz Holko <fastred@fastred.org>
2012-11-22 21:33:30 +0100

    Cleans up read map script

--- read_map.py
+++ read_map.py
@@ -1,20 +1,14 @@
+# Prints empty (colored in white) cell indices
+
 import Image
-im = Image.open("poland.png")
+im = Image.open("img/poland.png")
 rows = 40
 cols = 40
-#print dir(im)
 width, height = im.size
 colWidth = width/cols
 colHeight = height/rows
-#print colWidth
-#print colHeight
 for row in range(rows):
   for col in range(cols):
-    #pixels = [im.getpixel((x, y)) for x in range(col*colWidth,(col+1)*colWidth)
-        #for y in range(row*colHeight, (row+1)*colHeight)]
     pixel = im.getpixel((col*colWidth + colWidth/2, row*colHeight+colHeight/2))
     if pixel == (255, 255, 255, 255):
-      #print "col: " + str(col) + "\trow: " + str(row)
       print str(col + row * cols) + ", "
-    #print sum(pixels)
-
